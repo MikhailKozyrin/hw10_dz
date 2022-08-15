8c626ad8673f1bd1b4f74373389258f09b742f72
user <kozyan94@gmail.com>
2022-08-15 21:09:48 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author haioMiadzaki = new Author("Хаяо", "Миядзаки");
+        Book stranaChudes = new Book("Страна чудес без тормозов", 1987, haioMiadzaki);
+        Book stranaChudes2 = new Book("Страна чудес без тормозов", 1987, haioMiadzaki);
+
+        stranaChudes.setPublicationYear(1990);
+
+        System.out.println(haioMiadzaki);
+        System.out.println(stranaChudes);
+
+        System.out.println(stranaChudes.hashCode() == stranaChudes2.hashCode());
+        System.out.println(stranaChudes.equals(stranaChudes2));
+
 
     }
 }
